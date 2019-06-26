e6173c6687c78ecd0e47986330ebc28a8577aedd
subhahu123 <jainsubhahu@gmail.com>
2019-06-26 08:22:34 -0700

    settup is completes and now,.,,, d

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express') ;
 const app = express() ;
+const Mongoose = require("mongoose");
 
-app.listen(8080, "localhost", function() {
-    console.log("appt satr ") ;
+Mongoose.connect("mongodb://result:ipusem3@ds211029.mlab.com:11029/ipresults") ;
+
+app.listen( process.env.PORT || 8080, "localhost", function(data) {
+    console.log(data + "App is running at localhost:8080") ;
 }) ;
 
 app.get('/', function(req, res) {
