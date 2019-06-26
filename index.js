230f1acc128f2e91f9bf7a267141130c87aceee1
subhahu123 <jainsubhahu@gmail.com>
2019-06-26 13:38:42 -0700

    fixing the regex

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express') ;
 const app = express() ;
 const Mongoose = require("mongoose");
+const fs = require('fs') ;
 
-Mongoose.connect("mongodb://result:ipusem3@ds211029.mlab.com:11029/ipresults") ;
+// Mongoose.connect("mongodb://result:ipusem3@ds211029.mlab.com:11029/ipresults") ;
+
+const datapath = "./data.txt" ;
+
+pattern = /(\d+) ((\w+ )+)SID: .* SchemeID: .*(\r\n)+((\d+\(\d\) *)+)(\r\n)+(\d+ \d+ *)+(\r\n)+([0-9]+)(\r\n)+(\d+\(.+\) *)(\r\n)+/g ;
+
+
+fs.readFile('./data.txt', (err, data) => {
+    if(err) throw err ;
+    data = data.toString() ;
+    results = data.match(pattern) ;
+    console.log(results) ;
+    //console.log(data.toString()) ;
+})
+
+/* var text = fs.readFileSync(datapath).toString('utf-8');
+console.log(text) ;
+console.log(text.match(pattern)) ; */
+
 
 app.listen( process.env.PORT || 8080, "localhost", function(data) {
     console.log(data + "App is running at localhost:8080") ;
