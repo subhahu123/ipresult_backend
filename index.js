6c2d77c9a6e0811b3c8b72af02ccb50bc5466e61
subhahu123 <jainsubhahu@gmail.com>
2019-06-28 16:12:02 -0700

    add rest api

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express') ;
 const app = express() ;
 const Mongoose = require("mongoose");
-const fs = require('fs') ;
+const BodyParser = require("body-parser");
 
-// Mongoose.connect("mongodb://result:ipusem3@ds211029.mlab.com:11029/ipresults") ;
-
+Mongoose.connect("mongodb://result:ipusem3@ds211029.mlab.com:11029/ipresults") ;
 
 var studentSchema = new Mongoose.Schema({
     rollNo: String,
@@ -17,44 +16,18 @@ var studentSchema = new Mongoose.Schema({
 
 const studentModel = Mongoose.model("student", studentSchema);
 
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 
-const datapath = "./data.txt" ;
-
-pattern = /(\d+) ((\w+ )+)SID: .* SchemeID: .*(\r\n)+((\d+\(\d\) *)+)(\r\n)+(\d+ \d+ *)+(\r\n)+([0-9]+)(\r\n)+(\d+\(.+\) *)(\r\n)+/g ;
-
-
-fs.readFile('./data.txt', (err, data) => {
-    if(err) throw err ;
-    data = data.toString() ;
-    while( (results = pattern.exec(data)) != null ) {
-        var student = new studentModel();
-        student.rollNo = results[1] ;
-        student.name = results[2] ;
-        var arrSubjects = results[5].split(' ') ;
-        var arrMarks = results[12].split(' ') ;
-        for(var i = 0 ; i < arrMarks.length ; i++)
-            student.results.push({ subject: arrSubjects[i], marks: arrMarks[i] })
-        var log = student.save() ;
-
-       // console.log(log + "\n") ;
-        //console.log( "Roll No. :" + results[1] + " Name: " + results[2] +  " Subjects :" + results[5] + " " + results[8] + " " + results[10] + " Marks :" + results[12] ) ;
+app.get("/students", async (request, response) => {
+    try {
+        var result = await studentModel.find().exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
     }
-    //results = data.match(pattern) ;
-    /*results.forEach( element => {
-        console.log(element) ;
-    });*/
-    //console.log(data.toString()) ;
-})
-
-/* var text = fs.readFileSync(datapath).toString('utf-8');
-console.log(text) ;
-console.log(text.match(pattern)) ; 
-
+});
 
 app.listen( process.env.PORT || 8080, "localhost", function(data) {
     console.log(data + "App is running at localhost:8080") ;
 }) ;
-
-app.get('/', function(req, res) {
-    res.send({some: "hello worldl", SM: "AA"}) ;
-}) */
