811bff6ed53ba351bc82406a438bf3d90c40ef68
subhahu123 <jainsubhahu.gmail.com>
2019-07-05 17:36:55 +0530

    redesining the database

--- databasemongoose.js
+++ databasemongoose.js
@@ -1,10 +1,10 @@
-const express = require('express') ;
-const app = express() ;
+const express = require('express');
+const app = express();
 const Mongoose = require("mongoose");
-const fs = require('fs') ;
+const fs = require('fs');
 
 // Mongoose.connect("mongodb://result:ipusem3@ds211029.mlab.com:11029/ipresults") ;
-Mongoose.connect("mongodb://ip:results123@ds347367.mlab.com:47367/ipuresult") ;
+Mongoose.connect("mongodb://ip:results123@ds347367.mlab.com:47367/ipuresult");
 
 
 var studentSchema = new Mongoose.Schema({
@@ -29,33 +29,73 @@ const studentModel = Mongoose.model("3", studentSchema);
 //const studentModel = Mongoose.model("8", studentSchema);
 
 
-const datapath = "./ece.txt" ;
+const datapath = "./ece.txt";
 
-pattern = /(\d+) ((\w+ )+)SID: .* SchemeID: .*(\r\n)+((\d+\(\d\) *)+)(\r\n)+(\d+ \d+ *)+(\r\n)+([0-9]+)(\r\n)+(\d+\(.+\) *)(\r\n)+/g ;
+pattern = /(\d+) ((\w+ )+)SID: .* SchemeID: .*(\r\n)+((\d+\(\d\) *)+)(\r\n)+(\d+ \d+ *)+(\r\n)+([0-9]+)(\r\n)+(\d+\(.+\) *)(\r\n)+/g;
+
+var dir = './3'
+
+fs.readdir(dir, (err, list) => {
+    console.log(list.length);
+    list.forEach((file) => {
+        console.log(file);
+        fs.readFile(dir + '/' + file, (err, data) => {
+            if (err) throw err;
+            data = data.toString();
+            while ((results = pattern.exec(data)) != null) {
+                var student = new studentModel();
+                student.rollNo = results[1];
+                student.name = results[2];
+                var arrSubjects = results[5].split(' ');
+                var arrMarks = results[12].split(' ');
+                for (var i = 0; i < arrMarks.length; i++)
+                    student.results.push({
+                        subject: arrSubjects[i],
+                        marks: arrMarks[i]
+                    })
+                var log = student.save();
+
+                //console.log(log + "\n") ;
+                //    console.log( "Roll No. :" + results[1] + " Name: " + results[2] +  " Subjects :" + results[5] + " " + results[8] + " " + results[10] + " Marks :" + results[12] ) ;
+            }
+            //results = data.match(pattern) ;
+            /*results.forEach( element => {
+                console.log(element) ;
+            });*/
+            //console.log(data.toString()) ;
+        });
+    })
+})
 
 
-fs.readFile('./cse.txt', (err, data) => {
-    if(err) throw err ;
-    data = data.toString() ;
-    while( (results = pattern.exec(data)) != null ) {
+/*
+fs.readFile(dir + '/cse.txt', (err, data) => {
+    if (err) throw err;
+    data = data.toString();
+    while ((results = pattern.exec(data)) != null) {
         var student = new studentModel();
-        student.rollNo = results[1] ;
-        student.name = results[2] ;
-        var arrSubjects = results[5].split(' ') ;
-        var arrMarks = results[12].split(' ') ;
-        for(var i = 0 ; i < arrMarks.length ; i++)
-            student.results.push({ subject: arrSubjects[i], marks: arrMarks[i] })
-        var log = student.save() ; 
+        student.rollNo = results[1];
+        student.name = results[2];
+        var arrSubjects = results[5].split(' ');
+        var arrMarks = results[12].split(' ');
+        for (var i = 0; i < arrMarks.length; i++)
+            student.results.push({
+                subject: arrSubjects[i],
+                marks: arrMarks[i]
+            })
+        var log = student.save();
 
         //console.log(log + "\n") ;
-        console.log( "Roll No. :" + results[1] + " Name: " + results[2] +  " Subjects :" + results[5] + " " + results[8] + " " + results[10] + " Marks :" + results[12] ) ;
+        //    console.log( "Roll No. :" + results[1] + " Name: " + results[2] +  " Subjects :" + results[5] + " " + results[8] + " " + results[10] + " Marks :" + results[12] ) ;
     }
     //results = data.match(pattern) ;
     /*results.forEach( element => {
         console.log(element) ;
     });*/
-    //console.log(data.toString()) ;
-})
+//console.log(data.toString()) ;
+//   });
+
+
 
 /* var text = fs.readFileSync(datapath).toString('utf-8');
 console.log(text) ;
